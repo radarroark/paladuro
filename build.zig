e93aab25551da693a4a9541ad16965edb0caf9f1
radar roark <122068506+radarroark@users.noreply.github.com>
2025-08-08 10:40:46 -0400

    remove unused test command

--- build.zig
+++ build.zig
@@ -5,37 +5,22 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    {
-        const exe = b.addExecutable(.{
-            .name = "paladuro",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDeps(b, exe);
-        b.installArtifact(exe);
+    const exe = b.addExecutable(.{
+        .name = "paladuro",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    addDeps(b, exe);
+    b.installArtifact(exe);
 
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
-
-    {
-        const unit_tests = b.addTest(.{
-            .root_source_file = b.path("src/test.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
-        addDeps(b, unit_tests);
-
-        const run_unit_tests = b.addRunArtifact(unit_tests);
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&run_unit_tests.step);
-    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
 
 fn addDeps(b: *std.Build, step: *std.Build.Step.Compile) void {
--- src/test.zig
+++
@@ -1,4 +0,0 @@
-const std = @import("std");
-const main = @import("./main.zig");
-
-test "todo" {}
